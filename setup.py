2ad00b57863c58a56233f3818efc1d1099a905d6
ashish <apndey.15@gmail.com>
2016-01-11 22:45:44 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="history-exploiter",
+	version="1.1",
+	description="Lets you selectively manipulate the browsing history in Firefox",
+	url="https://github.com/ashish-pandey/HistoryExploiter.git",
+	author="Ashish Pandey",
+	author_email="apndey.15@gmail.com",
+	license='MIT',
+	packages=["history-exploiter"],
+	scripts=["bin/history-exploiter"],
+	zip_safe=False)
